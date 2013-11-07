f104d92e184bb02ed47e517865586da4a5f89587
unknown <plotkinm@.active.newpaltz.edu>
2013-11-07 16:20:04 -0500

    Click Highlight

--- Final/Views/Users/list.php
+++ Final/Views/Users/list.php
@@ -1,7 +1,7 @@
 <link href="//cdnjs.cloudflare.com/ajax/libs/datatables/1.9.4/css/jquery.dataTables.min.css" type="text/css" rel="stylesheet" />
 <style>
-	tr.success, tr.success td{
-		background-color: #00AA00;
+	.table tr.success2, .table tr.success2 td{
+		background-color: #FFAA00 !important; 
 	}
 </style>
 <div class="container">
@@ -53,6 +53,10 @@
 		$(".table").dataTable();
 		$(".alert .close").click(function(){
 			$(this).closest(".alert").slideUp();
+		});
+		
+		$(".table tr").click(function(){
+			$(this).toggleClass("success2");
 		});
 	})
 	</script>
